60ef18370a1a51ceb3b0730c921d852dabc87d9f
Herik25 <128887958+Herik25@users.noreply.github.com>
2023-07-04 12:36:14 +0530

    file name chnged, filter admin in different files

--- admin.php
+++ admin.php
@@ -46,99 +46,13 @@ if (isset($_POST['user']) && isset($_POST['pass'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./admin.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Karla&family=Luckiest+Guy&family=Open+Sans&family=Poppins&family=Ubuntu&display=swap" rel="stylesheet">
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <title>Document</title>
-    <style>
-        body{
-            background-color: #d9dddc;
-            overflow: hidden;
-        }
-        .container{
-            height: 100vh;
-            width: 100%;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-        }
-        .login{
-            display: flex;
-            flex-direction: column;
-            height: 350px;
-            width: 360px;
-            background-color: #e7e9ec;
-            align-items: center;
-            border-radius: 20px;
-            position: relative;
-        }
-        .title{
-            margin-top: 30px;
-            font-family: 'poppins' , sans-serif;
-            font-size: 40px;
-        }
-        .line{
-            display: inline;
-            width: 100%;
-            height: 2px;
-            background-color: #FFF;
-            z-index: 2;
-            margin-top: 30px;
-        }
-        input{
-            width: 100%;
-            padding: 12px;
-            margin-bottom: 30px;
-            border-radius: 10px;
-            outline: none;
-            border: none;
-        }
-        .inp{
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            margin-top: 40px;
-            position: relative;
-        }
-        .sub{
-            border-radius: 5px;
-            width: 60%;
-            padding: 5px;
-            font-size: 15px;
-            font-weight: 700;
-            font-family: sans-serif;
-            border: 2px solid #000;
-            background-color: #e7e9ec;
-        }
-        .sub:hover{
-            background-color: #555;
-            color: #fff;
-            transition: 0.3s;
-        }
-        .error{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            color: red;
-            font-size: 14px;
-            position: absolute;
-            bottom: 13%;
-            width: 300px;
-            font-family: 'Poppins', sans-serif;
-        }
-
-        .home{
-            position: absolute;
-            left: 2%;
-            top: 1%;
-            font-size: 30px;
-            cursor: pointer;
-        }
-        
-    </style>
 </head>
 <body>
     <div class="container">
@@ -168,25 +82,9 @@ if (isset($_POST['user']) && isset($_POST['pass'])) {
                 </form>
         </div>
     </div>
-    
-    <!-- <button id="home" onclick="goto()">HOME</button> -->
-    
     <script>
-        var login = document.getElementById('login')
-        var but = document.getElementById('home')
-        var sub = document.getElementById('sub')
-        // function goto(){
-        //     window.location.href = '/RMS'
-        // }
-        var log = "<?php echo $login ?>";
-        console.log(log);
-        if (log === "loged in") {
-            window.location.href = '/RMS/adminPanel.php';
-        }
-
-        function home(){
-            window.location.href = '/RMS/';
-        }
+       var log = "<?php echo $login ?>";
     </script>
+    <script src="./admin.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -31,7 +31,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./index.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Karla&family=Luckiest+Guy&family=Open+Sans&family=Poppins&family=Ubuntu&display=swap" rel="stylesheet">
@@ -169,6 +169,6 @@
    </div>
 
    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="./script.js"></script>
+    <script src="./index.js"></script>
 </body>
 </html>
